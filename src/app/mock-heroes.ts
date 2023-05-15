b3cd984b3937a87d15c413fd3d70afaf9fb5536e
Cookie Monster <cmonster@sesamestreet.org>
2023-05-15 12:26:53 +0200

    Update heroes.component and UI styles

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,23 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 12, name: 'Skarsnik'},
+  {id: 13, name: 'Headtaker'},
+  {id: 14, name: 'Ikit Claw'},
+  {id: 15, name: 'Karl Franz'},
+  {id: 16, name: 'Mannfred von Carstein'},
+  {id: 17, name: 'Heinrich Kemmler'},
+  {id: 18, name: 'Grimgor Ironhide'},
+  {id: 19, name: 'Ungrim Ironfist'},
+  {id: 20, name: 'Alberic de Bordeleaux'},
+  {id: 21, name: 'Abaddon the Despoiler'},
+  {id: 22, name: 'Ahriman'},
+  {id: 23, name: 'Kharn the Betrayer'},
+  {id: 24, name: 'Lucius the Eternal'},
+  {id: 25, name: 'Typhus'},
+  {id: 26, name: 'Fabius Bile'},
+  {id: 27, name: 'Asdrubael Vect'},
+  {id: 28, name: 'Ragnar Blackmane'},
+  {id: 29, name: 'Cato Sicarius'},
+  {id: 30, name: 'Frostblade'}
+];
