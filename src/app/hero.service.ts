11a9e05b0a0c9022878f20cdcdb81e47afb6674f
Cookie Monster <cmonster@sesamestreet.org>
2023-06-02 16:21:43 +0200

    Update hero service functions.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -45,28 +45,28 @@ export class HeroService {
       return of(result as T);
     };
   }
-  updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap(_ => this.log(`updated hero id=${hero.id}`)),
-      catchError(this.handleError<any>('updateHero'))
-    );
-  }
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
-  
-  addHero(hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
-      catchError(this.handleError<Hero>('addHero'))
-    );
-  }
-  deleteHero(id: number): Observable<Hero> {
-    const url = `${this.heroesUrl}/${id}`;
-
-    return this.http.delete<Hero>(url, this.httpOptions).pipe(
-      tap(_ => this.log(`deleted hero id=${id}`)),
-      catchError(this.handleError<Hero>('deleteHero'))
-      );
-  }
+//  updateHero(hero: Hero): Observable<any> {
+//    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+//      tap(_ => this.log(`updated hero id=${hero.id}`)),
+//      catchError(this.handleError<any>('updateHero'))
+//    );
+//  }
+//  httpOptions = {
+//    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+//  };
+//  
+//  addHero(hero: Hero): Observable<Hero> {
+//    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+//      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+//      catchError(this.handleError<Hero>('addHero'))
+//    );
+//  }
+//  deleteHero(id: number): Observable<Hero> {
+//    const url = `${this.heroesUrl}/${id}`;
+//
+//    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+//      tap(_ => this.log(`deleted hero id=${id}`)),
+//      catchError(this.handleError<Hero>('deleteHero'))
+//      );
+//  }
 }
