c1d08aca8ff89373dbd9f5c6b3ff324acff12330
Cookie Monster <cmonster@sesamestreet.org>
2023-06-15 12:34:24 +0200

    Added doc for hero.service.ts.
    The selected code is an Angular service called HeroService. This service contains methods for fetching, adding, updating, and deleting heroes.
    
    1. getHeroes(): Fetches an array of heroes from the remote API using the HttpClient. It uses RxJS pipe to log the action and handle errors.
    2. getHero(id: number): Fetches a single hero by id from the remote API. It constructs the URL by appending the id to the heroesUrl, and uses RxJS pipe to log the action and handle errors.
    3. log(message: string): A helper method that logs a message using the MessageService.
    4. heroesUrl: A string containing the remote API endpoint for heroes.
    5. handleError<T>(operation = 'operation', result?: T): A helper method that handles error scenarios derived from Observables. It logs the error and returns an Observable containing the default result value (if provided).
    6. updateHero(hero: Hero): Updates a hero using the HttpClient.put() method. It sends the updated hero object to the heroesUrl, and uses RxJS pipe to log the action and handle errors.
    7. httpOptions: An object containing HTTP headers to use for the requests. In this case, the Content-Type header is set to 'application/json'.
    8. addHero(hero: Hero): Adds a new hero using the HttpClient.post() method. It sends the new hero object to the heroesUrl, and uses RxJS pipe to log the action and handle errors.
    9. deleteHero(id: number): Deletes a hero by id using the HttpClient.delete() method. It constructs the URL by appending the id to the heroesUrl, and uses RxJS pipe to log the action and handle errors.
    
    The constructor of the HeroService has two dependencies: MessageService used for logging messages, and HttpClient used for making HTTP requests to the remote API.
    Here's a breakdown of its structure and methods:
    
    - @Injectable: This decorator marks the class as an Angular Service.
    - providedIn: 'root': Provides this service at the root level, making it a singleton throughout the app.
    - constructor: The constructor takes two services as its arguments, MessageService for logging and the HttpClient for making API requests.
    - getHeroes(): This method returns an Observable of an array of Hero objects. It fetches the heroes from the server using an HTTP GET request and logs the event.
    - getHero(id: number): This method gets a single hero from the server based on the given id. If the id is not found, the server will return a 404 error.
    - log(message: string): This method logs the given message using the MessageService.
    - handleError<T>(operation, result?): This method is a generic error handling function that logs the error and its related operation.
    - updateHero(hero: Hero): This function updates a hero's information by sending an HTTP PUT request to the server. It logs the event and handles errors.
    - addHero(hero: Hero): Adds a new hero by sending an HTTP POST request to the server. It logs the event and handles errors.
    - deleteHero(id: number): Deletes a hero with the given id by sending an HTTP DELETE request to the server. It logs the event and handles errors.
    
    Throughout the service, you'll also find the use of pipe, tap, and catchError RxJS operators to handle side effects and errors in a reactive and functional programming manner.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,11 @@ import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { catchError, map, tap } from "rxjs/operators";
 
+/**
+ * `HeroService` is an Injectable Angular service which provides methods for fetching, adding, updating, and deleting heroes.
+ * The service communicates with a remote API via `HttpClient` and stores retrieved heroes in an array.
+ * It also uses the `MessageService` to log the performed actions for later retrieval.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -26,7 +31,6 @@ export class HeroService {
  /** Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
-//    const hero = HEROES.find(h => h.id === id)!;
     return this.http.get<Hero>(url)
       .pipe(
         tap(_ => this.log(`fetched hero id=${id}`)),
